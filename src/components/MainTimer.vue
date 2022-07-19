c80f4510a4e43368791128dc1dc2bebf2be58796
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-07-19 06:47:24 -0300

    created a external component to Main Tracker

---
+++ src/components/MainTimer.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="column">
+        <div class="is-flex is-align-items-center is-justify-content-space-between">
+
+            <!-- Such as ReactJs, vue has a props inside its components, to declare the params it is just necessary to type -> :selectedParam="value" -->
+            <CountdownTimer :timeInSeconds="timeInSeconds" />
+
+            <!-- When this button is clicked the function inside @click will be called -->
+            <button class="button" @click="initiate">
+                <span class="icon">
+                    <i class="fas fa-play">
+                    </i>
+                </span>
+                <span>
+                    start
+                </span>
+            </button>
+
+            <button class="button" @click="end">
+                <span class="icon">
+                    <i class="fas fa-stop">
+                    </i>
+                </span>
+                <span>
+                    stop
+                </span>
+
+            </button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CountdownTimer  from './CountdownTimer.vue'
+
+export default defineComponent({
+  name: 'MainTimer',
+  components: {
+    CountdownTimer
+  },
+
+  // the initial state is defined here
+  data(){
+    // this return is always necessary!
+    return {
+      timeInSeconds: 0,
+      timeTracker: 0 
+    }
+
+  },
+
+
+  // all methods used in this components are difined here
+  methods: {
+    initiate(){
+      // 1s == 1000ms
+      this.timeTracker = setInterval(
+        () => {
+          this.timeInSeconds += 1
+        },
+        1000
+      )
+    },
+    end(){
+
+      clearInterval(this.timeTracker)
+
+    }
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  
+
+
+  
+</style>
+
