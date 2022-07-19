f19b75cb5c8a447428af4ea11be39308123b8ed9
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-07-19 06:30:24 -0300

    made the timeTracker function in a external component

---
+++ src/components/TimeTracker.vue
@@ -0,0 +1,50 @@
+<template>
+
+          <section>
+            <strong strong>{{ timePassed }}</strong>
+          </section>
+
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TimeTacker',
+
+  // the initial state is defined here
+  
+  //in props -> all recived values -> like ReactJs props or params in a function
+  props:{
+    timeInSeconds: {
+        type: Number,
+        default: 0,
+    }
+
+  },
+
+
+  computed: {// monitorate a information, if the information changes, it reacts 
+    /*It is declared as a method, but used as a property*/
+    timePassed () : string {
+      // 0 => '00:00:00'
+
+      const timeInMilliseconds = this.timeInSeconds * 1000
+      const timeInMillisecondsString = new Date(timeInMilliseconds).toISOString()
+
+
+     return timeInMillisecondsString.substr(11, 8) // returns only HH:MM:SS
+    }
+  },
+
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  
+
+
+  
+</style>
