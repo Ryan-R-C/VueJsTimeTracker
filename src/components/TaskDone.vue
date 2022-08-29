3711e9be2d64ede6d2d04ecd46ed5f4ac71ab950
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-08-29 19:55:41 -0300

    refractored the TaskDone Component to recieve the dynamic data

--- src/components/TaskDone.vue
+++ src/components/TaskDone.vue
@@ -2,22 +2,29 @@
     <section class="box has-text-weight-bold">
         <div class="columns">
             <div class="column is-7">
-                DDDDDD
+                {{task.description}}
             </div>
             <div class="column">
-                <CountdownTimer :timeInSeconds=15 />
+                <CountdownTimer :timeInSeconds=task.timeInSeconds />
             </div>
         </div>
     </section>
 </template>
   
 <script lang="ts">
-import { defineComponent } from 'vue';
+import ITask from '@/interfaces/ITask';
+import { defineComponent, PropType } from 'vue';
 import CountdownTimer from './CountdownTimer.vue';
   
   export default defineComponent({
     name: "TaskDone",
-    components: { CountdownTimer }
+    components: { CountdownTimer },
+    props: {
+        task: {
+            type: Object as PropType<ITask>,
+            required: true
+        }
+    }
 });
 </script>
 
