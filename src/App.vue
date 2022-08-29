1e574078585b2434eceae0eb50863c0158534330
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-08-29 19:57:30 -0300

    refractored the code to define the tasks into a side component

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,12 @@
       <SideBar/>
     </div>
     <div class="column is-three-quarter">
-      <FormTacker />
+      <FormTacker @toSaveTask="saveTask" />
+      <article class="list">
+        <TaskDone v-for="(task, index) in tasks" :key="index" :task="task"/>
+      </article>
     </div>
+
 
   </main>
 </template>
@@ -17,18 +21,37 @@ import { defineComponent } from 'vue';
 
 import SideBar from './components/SideBar.vue'
 import FormTacker from './components/FormTacker.vue'
-
+import TaskDone from './components/TaskDone.vue';
+import ITask from './interfaces/ITask'
 
 export default defineComponent({
   name: 'App',
+  // states from components
+  data(){
+    return{
+      tasks: [] as ITask[]
+    }
+  },
+
   // Necessary to define which components this component has as childs
   components: {
     SideBar,
-    FormTacker
+    FormTacker,
+    TaskDone
+  },
+  // all methods from this components
+  methods:{
+    saveTask(task: ITask){
+      this.tasks.push(task)
+    }
   }
 });
 </script>
 
 <style>
+  .list{
+    padding: 1.25rem;
+    /* margin: 10px; */
+  }
 
 </style>
