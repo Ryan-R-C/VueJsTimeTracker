9376a9de6c96c7226c5a26e341c4e598e9910623
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-08-29 19:37:58 -0300

    created the component to encapsulate the finished tasks

---
+++ src/components/TaskDone.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="box has-text-weight-bold">
+        <div class="columns">
+            <div class="column is-7">
+                DDDDDD
+            </div>
+            <div class="column">
+                <CountdownTimer :timeInSeconds=15 />
+            </div>
+        </div>
+    </section>
+</template>
+  
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CountdownTimer from './CountdownTimer.vue';
+  
+  export default defineComponent({
+    name: "TaskDone",
+    components: { CountdownTimer }
+});
+</script>
+
+<style scoped>
+    .box{
+        background-color: #FAF0CA;
+    }
+</style>
+  
+  
+  
