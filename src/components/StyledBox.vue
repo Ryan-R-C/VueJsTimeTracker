70ea67bd1f8248103e25b9f17df69ac7bf23a3de
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-09-03 15:25:20 -0300

    created a styled section component

---
+++ src/components/StyledBox.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="box has-text-weight-bold">
+        <slot>
+            <!-- 
+                ALL COMPONENT'S CHILDREN WILL BE DISPLAYED HERE
+             -->
+        </slot>
+    </section>
+</template>
+  
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: "StyledBox",
+});
+</script>
+
+<style scoped>
+    .box{
+        background-color: #FAF0CA;
+    }
+</style>
+  
+  
+  
